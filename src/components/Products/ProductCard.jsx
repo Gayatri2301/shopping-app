9ffe063dd4f49e0ec729815466e517569dac5588
Gayatri2301 <gayathriedibilli@gmail.com>
2024-06-23 12:07:24 +0530

    eye

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -39,12 +39,14 @@ const ProductCard = (props) => {
       <p>Rating: {props.rating.rate}</p>
       
       <div className='flex gap-2'>
+        <div>
       <button
-        className='bg-[#106F97] text-white p-2 mt-2'
+        className='bg-white text-white p-2 mt-2 border border-red-500 rounded'
         onClick={(event) => addWishlist(props.id, event)}
-      >
-        Add to Wishlist
-      </button>
+      ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-8 fill-red-500 hover:fill-red-600 absolute">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+    </svg><p className='text-black right-0'>Whishlist</p>
+      </button></div>
       <button className='bg-[#106F97] text-white p-2 mt-2'
       onClick={(e)=>addCart(props.id,e)}
       >Add to Cart</button>
