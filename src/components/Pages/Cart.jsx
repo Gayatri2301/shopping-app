71b9af0c79fd3f650482cceec0a181896e54ae6b
Gayatri2301 <gayathriedibilli@gmail.com>
2024-06-23 16:19:26 +0530

    wishlist icon

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -21,17 +21,17 @@ const Cart = () => {
     localStorage.setItem('wishList', JSON.stringify(temp));
   }
   return (
-    <div className='col-span-9 space-y-4'>
-      <p className='text-lg font-medium capitalize mb px-4 pt-4'>Items: {carts.length}</p>
+    <div className=''>
+      <p className=''>Items: {carts.length}</p>
       <ul>
-        <div className='flex items-center justify-between gap-6 p-4 border border-gray-200 rounded'>
+        <div className=''>
           {carts.map((item) => (
-            <div className='flex flex-col justify-between shadow-xl rounded-md p-5 w-[250px]' key={item.id} >
-              <div className='w-28 flex-shrink-0'>
+            <div className='' key={item.id} >
+              <div className=''>
               <img className='w-full' src={item.image} alt={item.title} />
               </div>
               <div className='w-1/3'>
-              <p className='text-gray-800 text-xl font-medium uppercase'>{item.title}</p>
+              <p className=''>{item.title}</p>
               <p>Price: ${item.price}</p>
               <p>Rating: {item.rating.rate}</p>
               </div>
